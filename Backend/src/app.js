af19417793d2f16db7b68d8d8f3614528d333120
sumita <sumi.sparkal@gmail.com>
2026-06-28 10:29:55 +0530

    Fix Vercel origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
    origin: [
         "http://localhost:5173",
-        "https://insightflow-ai-lake.vercel.app"
+       "https://insight-flow-ai-lake.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
